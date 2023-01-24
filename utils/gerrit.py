rleon/yo

commit 742c2b88646802781d6905ff7be861c99230ba15

yo: Add push command to the suite
Signed-off-by: Leon Romanovsky <leon@kernel.org>

utils/gerrit.py
@@ -1,0 +1,60 @@
+import os
+import tempfile
+import subprocess
+from utils.misc import in_directory
+from .git import *
+
+def get_gerrit_remote():
+    """TODO: support other profiles than kernel"""
+    remotes = git_output(["remote"]).decode().split()
+    for remote in remotes:
+        url = git_output(["remote", "get-url", "--push", remote]).decode()
+        if ":29418/upstream/linux" in url:
+            return remote
+
+def git_push_gerrit(remote, commit, branch, topic, dry_run=False):
+    cmd = ["push"]
+    if dry_run:
+        cmd += ["--dry-run"]
+
+    git_call(cmd + [remote, '%s:refs/for/%s%%topic=%s' % (commit, branch, topic)])
+
+def push_squash(remote, upto, base, branch, topic, issue, changeid, dry_run, verbose):
+    with tempfile.TemporaryDirectory() as d:
+        git_detach_workspace(d, verbose)
+
+        with in_directory(d):
+            git_reset_branch("%s/%s" % (remote, base), verbose)
+            log = git_simple_output(['log', '-n', '100', '--abbrev=12',
+                                     '--format=commit %h (\"%s\")', 'HEAD..', upto])
+
+            try:
+                git_merge_squash(upto, verbose)
+            except subprocess.CalledProcessError:
+                git_worktree_prune()
+                return
+
+            with tempfile.NamedTemporaryFile('w') as F:
+                F.write('%s testing\n\n%s\n\nIssue: %s\nChange-Id: %s\n'
+                        % (branch, log, issue, changeid))
+                F.flush()
+
+                try:
+                    git_commit_from_file(F.name, verbose)
+                except subprocess.CalledProcessError:
+                    # nothing to commit, working tree clean
+                    pass
+
+                try:
+                    git_push_gerrit(remote, "HEAD", base, topic, dry_run)
+                except subprocess.CalledProcessError:
+                    # nothing to commit, working tree clean
+                    pass
+
+    git_worktree_prune()
+
+def generate_changeid(branch="HEAD"):
+    ps = subprocess.run(["git", "show", branch], check=True, capture_output=True)
+    patchid = subprocess.run(["git", "patch-id", "--stable"], input=ps.stdout, capture_output=True)
+
+    return patchid.stdout.decode('utf-8').strip().split()[0]

plugins/cmd_push.py
@@ -1,0 +1,89 @@
+"""YO cloud tools
+"""
+from utils.git import *
+from utils.gerrit import *
+from utils.cache import get_branch_cache
+from profiles import get_config
+
+#--------------------------------------------------------------------------------------------------------
+def args_push(parser):
+    parser.add_argument(
+            "-s",
+            "--squash",
+            dest="squash",
+            action="store_true",
+            help="Squash all commits into one patch",
+            default=False)
+    parser.add_argument(
+            "-n",
+            "--dry-run",
+            dest="dry_run",
+            action="store_true",
+            help="dry run",
+            default=False)
+    parser.add_argument(
+            "-v",
+            "--verbose",
+            dest="verbose",
+            action="store_true",
+            help="Be more verbose",
+            default=False)
+    parser.add_argument(
+            "--upto",
+            dest="upto",
+            nargs=1,
+            help="Push upto to this specific commit")
+    parser.add_argument(
+            "--topic",
+            dest="topic",
+            nargs=1,
+            help="Push to this specific topic")
+    parser.add_argument(
+            "--base",
+            dest="base",
+            nargs=1,
+            help="Base push on specific branch")
+
+def cmd_push(args):
+    """Push to gerrit"""
+
+    args.root = git_root()
+    if args.root is None:
+        exit()
+
+    config = get_config("push")
+    remote = get_gerrit_remote()
+
+    git_remote_update([remote])
+    branch = git_current_branch()
+    cache = get_branch_cache(branch)
+
+    if not args.base:
+        bases = ["rdma-rc-mlx", "rdma-next-mlx", "net-next", "net"]
+        base = git_find_base(remote, branch, bases)
+    else:
+        base = args.base[0]
+
+    if not args.topic:
+        args.topic=branch;
+    else:
+        args.topic=args.topic[0]
+
+    if not args.upto:
+        args.upto = git_current_sha()
+    else:
+        args.upto = args.upto[0]
+
+    issue = cache['issue']
+    changeid = cache['changeid']
+
+    if args.squash:
+        push_squash(remote, args.upto, base, branch, args.topic,
+                    issue, changeid, args.dry_run, args.verbose)
+        return
+
+    try:
+        git_push_gerrit(remote, args.upto, base, args.topic, args.dry_run)
+    except subprocess.CalledProcessError:
+        # nothing to commit, working tree clean
+        pass

plugins/cmd_verify.py
@@ -3,19 +3,9 @@
 import os
 import tempfile
 from utils.git import *
+from utils.gerrit import get_gerrit_remote, git_push_gerrit
+from utils.misc import in_directory
 from profiles import get_config
-from contextlib import contextmanager
-
-@contextmanager
-def in_directory(dir):
-    """Context manager that chdirs into a directory and restores the original
-    directory when closed."""
-    cdir = os.getcwd()
-    try:
-        os.chdir(dir)
-        yield True
-    finally:
-        os.chdir(cdir)
 
 def buiild_commit(remote, branch, changeid, bases, verbose, dry_run):
     base_branch = git_find_base(remote, branch, bases)
@@ -30,7 +20,7 @@ def buiild_commit(remote, branch, changeid, bases, verbose, dry_run):
         return
 
     with tempfile.NamedTemporaryFile('w') as F:
-        F.write('%s testing\n\n%s\n\nIssue: 1308201\nChange-Id: %s\nSigned-off-by: Leon Romanovsky <leonro@nvidia.com>' % (branch, log, changeid))
+        F.write('%s testing\n\n%s\n\nIssue: 1308201\nChange-Id: %s\n' % (branch, log, changeid))
         F.flush()
 
         try:
@@ -40,7 +30,7 @@ def buiild_commit(remote, branch, changeid, bases, verbose, dry_run):
             pass
 
         try:
-            git_push_gerrit(remote, "HEAD", base_branch, dry_run)
+            git_push_gerrit(remote, "HEAD", base_branch, "leon_testing", dry_run)
         except subprocess.CalledProcessError:
             # nothing to commit, working tree clean
             pass
@@ -58,14 +48,6 @@ def buiild_commits(remote, branches, bases, current_changeid=False,
                     buiild_commit(remote, branch, changeid, bases, verbose, dry_run)
 
     git_worktree_prune()
-
-def get_verify_remote():
-    """TODO: support other profiles than kernel"""
-    remotes = git_output(["remote"]).decode().split()
-    for remote in remotes:
-        url = git_output(["remote", "get-url", "--push", remote]).decode()
-        if "l-gerrit.mtl.labs.mlnx:29418/upstream/linux" in url:
-            return remote
 
 #--------------------------------------------------------------------------------------------------------
 def args_verify(parser):
@@ -106,7 +88,7 @@ def cmd_verify(args):
         exit()
 
     config = get_config("verify")
-    remote = get_verify_remote()
+    remote = get_gerrit_remote()
 
     git_remote_update([remote])
     if args.current:

utils/cache.py
@@ -1,0 +1,35 @@
+import os
+import tomllib
+import subprocess
+from .misc import in_directory
+from .gerrit import generate_changeid
+
+cache_dir = os.path.expanduser("~/.cache/yo")
+
+def create_cache_dir():
+    if not os.path.exists(cache_dir):
+        os.makedirs(cache_dir)
+
+def add_branch_cache(f, branch, issue=1308201):
+     with open(f, mode="a", encoding="utf-8") as fp:
+            fp.write("[\"" + branch + "\"]\n")
+            fp.write("issue = %d\n" % (issue))
+            fp.write("changeid = \"%s\"\n" % (generate_changeid(branch)))
+
+
+def get_branch_cache(branch):
+    create_cache_dir()
+    f = cache_dir + "/push.toml"
+
+    if not os.path.isfile(f):
+        add_branch_cache(f, branch)
+
+    with open(f, mode="rb") as fp:
+        cache = tomllib.load(fp)
+
+    try:
+        return cache[branch]
+    except KeyError:
+        add_branch_cache(f, branch)
+        with open(f, mode="rb") as fp:
+            return tomllib.load(fp)[branch]

utils/git.py
@@ -80,13 +80,6 @@ def git_push(remote, branches, force=False, dry_run=False):
 
     git_call(cmd + [remote] + branches)
 
-def git_push_gerrit(remote, commit, branch, dry_run=False):
-    cmd = ["push"]
-    if dry_run:
-        cmd += ["--dry-run"]
-
-    git_call(cmd + [remote, '%s:refs/for/%s%%topic=leon_testing' % (commit, branch)])
-
 def git_same_content(a, b):
     if a == b:
         return True
@@ -134,6 +127,9 @@ def git_reset_branch(commit, verbose=False):
 
 def git_current_branch():
     return git_output(["symbolic-ref", "--short", "-q", "HEAD"]).decode()
+
+def git_current_sha():
+    return git_output(["rev-parse", "--short", "-q", "HEAD"]).decode()
 
 def git_checkout_branch(branch=None, verbose=False):
     """Checkout specific branch and return previous branch"""
@@ -232,6 +228,12 @@ def git_worktree_prune():
 def git_commit_from_file(fname, verbose=False):
     cmd = ["commit", "--allow-empty", "--no-edit"]
     if not verbose:
-        git_output(cmd + ["-q", "-F", fname])
+        git_output(cmd + ["-q", "-s", "-F", fname])
     else:
-        git_call(cmd + ["-F", fname])
+        git_call(cmd + ["-s", "-F", fname])
+
+def git_author_name():
+    git_simple_output(["config", "user.name"])
+
+def git_author_email():
+    git_simple_output(["config", "user.email"])

utils/misc.py
@@ -1,5 +1,17 @@
 import os
 import subprocess
+from contextlib import contextmanager
+
+@contextmanager
+def in_directory(dir):
+    """Context manager that chdirs into a directory and restores the original
+    directory when closed."""
+    cdir = os.getcwd()
+    try:
+        os.chdir(dir)
+        yield True
+    finally:
+        os.chdir(cdir)
 
 project_marks = {
         "kernel": "scripts/faddr2line",

yo
@@ -13,6 +13,7 @@ cmd_modules = {
     "cmd_setup",
     "cmd_update",
     "cmd_upload",
+    "cmd_push",
 }
 
 if __name__ == "__main__":

commit 7ef7953811769165b7c5565a4d938039159824e5

push: Fix changeid generation
Change-Id needs to be in Ixxx.. format, so fix it.

Fixes: 742c2b886468 ("yo: Add push command to the suite")
Signed-off-by: Leon Romanovsky <leon@kernel.org>

utils/gerrit.py
@@ -57,4 +57,4 @@ def generate_changeid(branch="HEAD"):
     ps = subprocess.run(["git", "show", branch], check=True, capture_output=True)
     patchid = subprocess.run(["git", "patch-id", "--stable"], input=ps.stdout, capture_output=True)
 
-    return patchid.stdout.decode('utf-8').strip().split()[0]
+    return "I%s" % (patchid.stdout.decode('utf-8').strip().split()[0])