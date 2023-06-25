1efdf9ad35d8945d39b750027c54fdee83f0a99b
Leon Romanovsky <leon@kernel.org>
2023-06-25 13:35:03 +0300

    cloud: Factor out common cloud API code
    Move common cloud API code to separate utils file.
    
    Signed-off-by: Leon Romanovsky <leon@kernel.org>

--- plugins/cmd_cloud.py
+++ plugins/cmd_cloud.py
@@ -1,6 +1,6 @@
 """YO cloud tools
 """
-from utils.cache import get_cloud_cache
+from utils.cloud import get_user_sessions, get_base_headers, get_players_data
 
 import math
 import requests
@@ -51,44 +51,7 @@ def extend_setups(r, headers):
     print(t.draw())
 
 def restart_vm(r, headers, n):
-    name = None
-    for setups in r.json():
-        if setups['status'] != 'active':
-            continue
-
-        if name is not None and n is None:
-            exit("You have more than one setup. Please specify VM to restart")
-
-        name = setups['setup_info']['name']
-        # n can be provided in one of the following formats:
-        # 1. Setup name, e.g. 10.141.67.105-106_cx4
-        # 2. Specific IP
-        ip1=name.split('-')[0]
-        try:
-            ip2='.'.join(ip1.split('.')[:-1]) + '.' + name.split('-')[1].split('_')[0]
-        except IndexError:
-            # We have single node machine
-            ip2=ip1
-        if n is not None and name != n and ip1 != n and ip2 != n:
-            name = None
-            continue
-
-        _id = setups['_id']
-        host = setups['setup_info']['players'][0]['host_ip']
-        players=[]
-        if name == n:
-            players = ['player1', 'player2']
-        if ip1 == n:
-            players = ['player1']
-        if ip2 == n:
-            players = ['player2']
-        if n is not None:
-            break
-
-    if name is None:
-        print("There are no setups to restart")
-        return
-
+    _id, players, host = get_players_data(r, n)
     data = {"players_info": {host: players}}
     ext = requests.post('http://linux-cloud.mellanox.com/api/session/reload_setup/%s' %(_id),
                         json=data, headers=headers)
@@ -129,14 +92,8 @@ def args_cloud(parser):
 def cmd_cloud(args):
     """Manage sessions"""
 
-    cache = get_cloud_cache()
-
-    headers = {'Cache-Control' : 'no-cache'}
-    headers.update({'User-Agent' : 'Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/111.0'})
-    headers.update({'Referer' : 'http://linux-cloud.mellanox.com/user_sessions'})
-    headers.update({'Cookie': cache['cookie']})
-    payload = {'user': '', 'limit': '0'}
-    r = requests.get('http://linux-cloud.mellanox.com/api/get_user_sessions', params=payload, headers=headers)
+    r = get_user_sessions()
+    headers = get_base_headers()
 
     if args.extend:
         extend_setups(r, headers)
---
+++ utils/cloud.py
@@ -0,0 +1,63 @@
+"""YO cloud tools
+"""
+from utils.cache import get_cloud_cache
+import requests
+
+def get_players_data(r, n):
+    name = None
+    for setups in r.json():
+        if setups['status'] != 'active':
+            continue
+
+        if name is not None and n is None:
+            exit("You have more than one setup. Please specify VM to restart")
+
+        name = setups['setup_info']['name']
+        # n can be provided in one of the following formats:
+        # 1. Setup name, e.g. 10.141.67.105-106_cx4
+        # 2. Specific IP
+        ip1=name.split('-')[0]
+        try:
+            ip2='.'.join(ip1.split('.')[:-1]) + '.' + name.split('-')[1].split('_')[0]
+        except IndexError:
+            # We have single node machine
+            ip2=ip1
+        if n is not None and name != n and ip1 != n and ip2 != n:
+            name = None
+            continue
+
+        _id = setups['_id']
+        host = setups['setup_info']['players'][0]['host_ip']
+        players=[]
+        if name == n:
+            players = ['player1', 'player2']
+        if ip1 == n:
+            players = ['player1']
+        if ip2 == n:
+            players = ['player2']
+        if n is not None:
+            break
+
+    if name is None:
+        exit("There are no setups with this info")
+
+    return _id, players, host
+
+def get_base_headers():
+    cache = get_cloud_cache()
+
+    headers = {'Cache-Control' : 'no-cache'}
+    headers.update({'User-Agent' : 'Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/111.0'})
+    headers.update({'Referer' : 'http://linux-cloud.mellanox.com/user_sessions'})
+    headers.update({'Cookie': cache['cookie']})
+
+    return headers
+
+def get_user_sessions():
+    """Get user sessions"""
+
+    headers = get_base_headers()
+
+    payload = {'user': '', 'limit': '0'}
+    return requests.get('http://linux-cloud.mellanox.com/api/get_user_sessions', 
+                        params=payload, headers=headers)
