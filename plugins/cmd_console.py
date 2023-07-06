898b94d32484076a2b929d6de075041a3034b123
Leon Romanovsky <leon@kernel.org>
2023-07-06 13:56:04 +0300

    console: Add an option to select from existing setups
    Use python questionary module to allow "yo console .." execution without
    and parameters, so users will be able to chose relevant setup to
    connect.
    
    ➜  kernel git:(tmp-fix) yo console
    ? Which server to connect? (Use arrow keys)
     » 1.2.1.145    debug RM3497850
       1.2.1.146    debug RM3497850
       ---------------
    
    Signed-off-by: Leon Romanovsky <leon@kernel.org>

--- plugins/cmd_console.py
+++ plugins/cmd_console.py
@@ -1,6 +1,27 @@
 """YO cloud tools
 """
+from utils.cloud import get_user_sessions, get_base_headers
+
 import os
+import questionary
+
+def get_players_info(r):
+    headers = get_base_headers()
+    host = []
+    for setups in r.json():
+        if setups['status'] != 'active':
+            continue
+
+        name = setups['setup_info']['name']
+
+        host += [setups['setup_info']['players'][0]['ip'] +
+                 '    ' + setups['session_description']]
+        if len(setups['setup_info']['players']) == 2:
+            host += [ setups['setup_info']['players'][1]['ip'] +
+                     '    ' + setups['session_description']]
+        host += [questionary.Separator()]
+
+    return host
 
 #--------------------------------------------------------------------------------------------------------
 def args_console(parser):
@@ -13,7 +34,10 @@ def args_console(parser):
 def cmd_console(args):
     """View console"""
     if args.name is None:
-        exit("Please specify server name")
+        r = get_user_sessions()
+        players = get_players_info(r)
+        choice = questionary.select("Which server to connect?", players).ask()
+        args.name = choice.split(' ')[0]
 
     cmd = ['ssh', '-t', ] + [args.name] + ['dmesg -w']
     os.execvp(cmd[0], cmd)
