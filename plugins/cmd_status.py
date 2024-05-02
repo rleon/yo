4c50643da3613368d551a6b9f34d5f9c1788acea
Leon Romanovsky <leon@kernel.org>
2024-05-02 15:12:02 +0300

    status: Add new command to see patch status in the given branch
    ➜  kernel git:(m/dma-v1) yo status
    +---------+-----------------------------------------------------+----------------------+----+----+----+----+
    | Issue   | Subject                                             | Updated              | BT | CA | KC | RT |
    +=========+=====================================================+======================+====+====+====+====+
    | 1083426 | RDMA/umem: Store ODP access mask information in PFN | 15:03:47 02-05-2024  |    | -1 | 1  |    |
    +---------+-----------------------------------------------------+----------------------+----+----+----+----+
    | 673122  | iommu/dma: Implement link/unlink ranges callbacks   | 15:00:44 02-05-2024  |    | 1  | 1  |    |
    +---------+-----------------------------------------------------+----------------------+----+----+----+----+
    ....
    
    Signed-off-by: Leon Romanovsky <leon@kernel.org>

---
+++ plugins/cmd_status.py
@@ -0,0 +1,78 @@
+"""YO cloud tools
+"""
+from utils.git import *
+from utils.gerrit import *
+from utils.cache import get_branch_cache
+from utils.misc import get_project
+from profiles import get_config
+from texttable import Texttable
+from datetime import datetime
+
+import vendor.gerrit as gerrit
+
+def print_patches_status(remote, changeid):
+    t = Texttable(max_width=0)
+    t.set_header_align(["l", "l", "l", "l", "l", "l", "l"])
+    t.header(('Issue', 'Subject', 'Updated', 'BT', 'CA', 'KC', 'RT'))
+
+    host, port, project = get_gerrit_info(remote)
+    rev = gerrit.Query(host, port)
+    to_filter = [gerrit.OrFilter().add_items('change', changeid)]
+    other = gerrit.Items()
+    other.add_items('project', project)
+    other.add_items('limit', len(changeid))
+
+    to_filter.append(other)
+    data = []
+    for review in rev.filter(*to_filter):
+        last_updated = datetime.fromtimestamp(review['lastUpdated'])
+        last_updated = last_updated.strftime("%H:%M:%S %d-%m-%Y ")
+
+        approvals = review['currentPatchSet']['approvals']
+        kc = ' '
+        ca = ' '
+        bt = ' '
+        rt = ' '
+        for d in approvals:
+            if d['type'] == 'Kernel-Compile':
+                kc = d['value']
+
+            if d['type'] == 'Code-Analysis':
+                ca = d['value']
+
+            if d['type'] == 'Bluefield-Tests':
+                bt = d['value']
+
+            if d['type'] == 'Regression-Tests':
+                rt = d['value']
+
+        data.append((review['number'], review.get('subject'),
+                     last_updated, bt, ca, kc, rt))
+
+    t.add_rows(data, False)
+    print(t.draw())
+
+#--------------------------------------------------------------------------------------------------------
+def args_status(parser):
+    pass
+
+def cmd_status(args):
+    """Patches status"""
+
+    args.root = git_root()
+    if args.root is None:
+        exit()
+
+    args.project = get_project(args)
+    if args.project != "kernel":
+        exit("Upload is supported for kernel tree only.")
+
+    remote = get_gerrit_remote()
+    log = git_simple_output(["log", "-50"])
+    changeid = []
+    for line in log.splitlines():
+        if "Change-Id" in line:
+            line = line.split(':')
+            changeid += [line[1].strip()]
+
+    print_patches_status(remote, changeid)
