a8e3a7797ab4d1b9705f27f5361ca0a97a5d18ac
Leon Romanovsky <leon@kernel.org>
2024-11-19 05:48:52 -0500

    Revert "upload: Update cmd_upload to my new setup"
    This reverts commit 94fbc48a477a0c6c1e851781fd8826080e5e2c6c.

--- plugins/cmd_upload.py
+++ plugins/cmd_upload.py
@@ -44,7 +44,7 @@ def cmd_upload(args):
         exit("Upload is supported for kernel tree only.")
 
     config = get_config("upload")
-    remotes = list(config["no_force"].keys()) + list(config["force"].keys()) + list(config["extra"]["remotes"])
+    remotes = list(config["no_force"].keys()) + list(config["force"].keys())
     if not args.no_testing:
         remotes += list(config["cross_check"].keys())
         remotes += list(config["testing"].keys())
